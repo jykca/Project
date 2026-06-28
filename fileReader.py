4e1074fbe54ee3fd8c3fd7f2a4aa358333233cb3
Jacky <jykcajj@gmail.com>
2026-06-27 23:02:43 -0600

    made it so python can talk to arduino

--- fileReader.py
+++ fileReader.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial("COM5", 115200)
+ser = serial.Serial("COM5", 9600)
 
 time.sleep(2)
 
@@ -9,7 +9,15 @@ time.sleep(2)
 
 with open("paths/circle.txt", "r") as file:
     for line in file:
+        print("SEND:", line.strip())
         ser.write(line.encode())   
+
         time.sleep(0.05)        
+
+        start = time.time()
+        while time.time() - start < 0.3:
+            msg = ser.readline().decode(errors="ignore").strip()
+            if msg:
+                print("ARDUINO:", msg)
               
 ser.close()
